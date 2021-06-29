a44c0b85947fc49cabda77ba7abeb82542c4dfe5
cphork <christina.phork@gmail.com>
2021-06-28 23:52:25 -0500

    buttons spaced

--- js/app.js
+++ js/app.js
@@ -48,11 +48,11 @@ $.ajax("https://spreadsheets.google.com/feeds/list/1lLER4scBm67_Kh6aY1WIif3ORhnK
             <div class="card ml-2 mt-8 w-74 lg:w-56">
             <img class='border border-gray-600 ' src="${project.image}" class="card-img-top" alt="project.image">
             <div class="card-body border border-gray-600">
-                <h3 class="card-title text-blue-500">${project.title}</h3>
-                <h5 class="card-description text-blue-500">${project.description}</h5>
-                <div class='mt-8 ml-32 lg:flex flex-col'>
-                <a href="${project.liveURL}" class="btn btn-primary bg-green-200 text-black lg:mb-2 lg:text-sm">Live</a>
-                <a href="${project.gitURL}" class="btn btn-primary bg-green-200 text-black lg:mb-2 lg:text-sm">Git</a>
+                <h3 class="card-title text-blue-500 text-center">${project.title}</h3>
+                <h5 class="card-description text-blue-500 text-center">${project.description}</h5>
+                <div class='mt-8 ml-32 lg:flex justify-center lg:mr-56'>
+                <a href="${project.liveURL}" class="btn btn-primary bg-green-200 text-black lg:mb-2 lg:text-sm lg:mr-4">Live</a>
+                <a href="${project.gitURL}" class="btn btn-primary bg-green-200 text-black lg:mb-2 lg:text-sm lg:mr-16">Git</a>
                 </div>
             </div>
         </div>
